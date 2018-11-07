d653ceaa4daa26e397ab2c1d577f3f9beb718310
Sam Xia <pot365@gmail.com>
2018-11-07 21:32:54 +0800

    eee

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+   
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.include?("?") == true
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
